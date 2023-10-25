d6b3eac1b5371656bd57a8a16fdb246176dd4e4c
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-10-25 21:02:37 +0800

    Build the Navbar component.

---
+++ components/main-nav.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { Category } from "@/types";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function MainNav({ data }: { data: Category[] }) {
+  const pathname = usePathname();
+
+  const routes = data.map((route) => ({
+    href: `/category/${route.id}`,
+    label: route.name,
+    active: pathname === `/category${route.id}`,
+  }));
+
+  return (
+    <nav className="mx-6 flex items-center space-x-4 lg:space-x-6">
+      {routes.map((route) => (
+        <Link
+          key={route.href}
+          href={route.href}
+          className={cn(
+            "text-sm font-medium transition-colors hover:text-primary hover:bg-accent hover:rounded p-1",
+            route.active
+              ? "text-primary rounded bg-secondary dark:text-white"
+              : "text-muted-foreground"
+          )}
+        >
+          {route.label}
+        </Link>
+      ))}
+    </nav>
+  );
+}
---
+++ components/navbar.tsx
@@ -0,0 +1,18 @@
+import MainNav from "@/components/main-nav";
+import Container from "@/components/ui/container";
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <div className="border-b">
+      <Container>
+        <div className="relative px-4 sm:px-6 lg:px-8 flex items-center h-16">
+          <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
+            <p className="font-bold text-xl">STORE</p>
+          </Link>
+          <MainNav data={[]} />
+        </div>
+      </Container>
+    </div>
+  );
+}
