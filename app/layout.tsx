3ce162ed36ca5c917f30ad9df71a5405ee5528df
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-10-25 20:09:39 +0800

    Initial Commit

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Urbanist } from "next/font/google";
+import "./globals.css";
+
+const font = Urbanist({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "E-commerce Store",
+  description: "E-commerce Store",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>{children}</body>
+    </html>
+  );
+}
