112565cd1082d47ef3eb9d27fd2d25449241a4d6
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-10-25 23:25:14 +0800

    Create ProductList component.

---
+++ components/product-list.tsx
@@ -0,0 +1,23 @@
+import NoResults from "@/components/ui/no-results";
+import ProductCard from "@/components/ui/product-card";
+import { Product } from "@/types";
+
+export default function ProductList({
+  title,
+  products,
+}: {
+  title: string;
+  products: Product[];
+}) {
+  return (
+    <div className="space-y-4">
+      <h3 className="font-bold text-3xl">{title}</h3>
+      {products.length === 0 && <NoResults />}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {products.map((product) => (
+          <ProductCard key={product.id} data={product} />
+        ))}
+      </div>
+    </div>
+  );
+}
