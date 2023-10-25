4a98c25b32ebb73e8fb5938268239933155ec62f
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-10-25 23:26:33 +0800

    Build Featured Products.

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,6 +1,7 @@
 import getBillboards from "@/actions/get-billboards";
 import getProducts from "@/actions/get-products";
 import Billboard from "@/components/billboard";
+import ProductList from "@/components/product-list";
 import Container from "@/components/ui/container";
 
 export const revalidate = 0;
@@ -12,6 +13,9 @@ export default async function HomePage() {
     <Container>
       <div className="space-y-10 pb-10">
         <Billboard data={billboard} />
+      </div>
+      <div className="flex flex-col gap-y-8 px-4 sm:px-4 lg:px-8">
+        <ProductList title="Featured Products" products={products} />
       </div>
     </Container>
   );
