3f2aad216e62d54bbf4f3b53a973b179b1f1e993
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-10-25 22:23:38 +0800

    Create getBillboard function.

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,13 +1,15 @@
+import getBillboards from "@/actions/get-billboards";
 import Billboard from "@/components/billboard";
 import Container from "@/components/ui/container";
 
 export const revalidate = 0;
 
-export default function HomePage() {
+export default async function HomePage() {
+  const billboard = await getBillboards("4e950028-0718-4d42-a27b-e04784107e8e");
   return (
     <Container>
       <div className="space-y-10 pb-10">
-        <Billboard data={[]} />
+        <Billboard data={billboard} />
       </div>
     </Container>
   );
