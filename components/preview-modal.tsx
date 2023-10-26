d1cf004ee837e5d0f4cfab489c9cef3a1c5f1cd8
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-10-26 23:06:59 +0800

    Update

--- components/preview-modal.tsx
+++ components/preview-modal.tsx
@@ -3,6 +3,7 @@
 import Gallery from "@/components/gallery/gallery-tab";
 import Modal from "@/components/ui/modal";
 import usePreviewModal from "@/hooks/usePreviewModal";
+import Info from "./info";
 
 export default function PreviewModal() {
   const previewModal = usePreviewModal();
@@ -15,6 +16,9 @@ export default function PreviewModal() {
       <div className="w-full items-start gap-x-6 gap-y-8 grid grid-cols-1 sm:grid-cols-12 lg:gap-x-8">
         <div className="sm:col-span-4 lg:col-span-5">
           <Gallery images={product.images} />
+        </div>
+        <div className="sm:col-span-8 lg:col-span-7">
+          <Info product={product} />
         </div>
       </div>
     </Modal>
