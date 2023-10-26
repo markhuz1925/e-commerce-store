7f5a47442b4e6ebc704081fb03869fb8138ee98c
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-10-26 21:14:08 +0800

    Reverse the array of routes.

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -16,7 +16,7 @@ export default function MainNav({ data }: { data: Category[] }) {
 
   return (
     <nav className="mx-6 flex items-center space-x-4 lg:space-x-6">
-      {routes.map((route) => (
+      {routes.reverse().map((route) => (
         <Link
           key={route.href}
           href={route.href}
